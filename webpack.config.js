35405f1f503045f89546914142ae3ee0d5ea66de
jeankxd <misterio_271@hotmail.com>
2018-01-30 13:29:32 -0500

    New

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/assets/js'),
+      '#': path.resolve(__dirname, 'resources/assets/sass')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  }
+}
+
+// test specific setups
+if (process.env.NODE_ENV === 'test') {
+  module.exports.externals = [require('webpack-node-externals')()]
+  module.exports.devtool = 'inline-cheap-module-source-map'
+}
